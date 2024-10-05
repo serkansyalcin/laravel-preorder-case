483e020ee56c2c4d3eeb84cc069b95f41871b6d4
serkansyalcin <serkan.syalcin@hotmail.com>
2024-10-05 17:29:55 +0700

    admin : finishing order Lists

--- resources/js/pages/admin/orders/OrderLists.vue
+++ resources/js/pages/admin/orders/OrderLists.vue
@@ -1,7 +1,7 @@
 <template>
     <h2 class="text-2xl font-bold text-gray-800 mb-6">Orders</h2>
 
-    <!-- Products Table -->
+    <!-- orders Table -->
     <div class="overflow-x-auto">
         <table class="min-w-full bg-white">
             <thead>
@@ -27,28 +27,27 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(product, index) in products" :key="product.id" class="hover:bg-gray-100">
+                <tr v-for="(order, index) in orders" :key="order.id" class="hover:bg-gray-100">
                     <td class="px-6 py-4 border-b border-gray-300">{{ index + 1 }}</td>
-                    <td class="px-6 py-4 border-b border-gray-300">{{ product.name }}</td>
-                    <td class="px-6 py-4 border-b border-gray-300">${{ product.total.toFixed(2) }}</td>
-                    <td class="px-6 py-4 border-b border-gray-300">{{ product.total_item }} items</td>
+                    <td class="px-6 py-4 border-b border-gray-300">{{ order.name }}</td>
+                    <td class="px-6 py-4 border-b border-gray-300">${{ order.total.toFixed(2) }}</td>
+                    <td class="px-6 py-4 border-b border-gray-300">{{ order.total_item }} items</td>
                     <td class="px-6 py-4 border-b border-gray-300">
-                        <span class="px-2 py-2 bg-blue-500 text-white rounded-md">{{ product.status }}</span>
+                        <span class="px-2 py-2 bg-blue-500 text-white rounded-md">{{ order.status }}</span>
                     </td>
                     <td class="px-6 py-4 border-b border-gray-300">
-                        <button @click="showConfirmDelete(product)" class="text-blue-500 hover:text-blue-700">
+                        <button @click="showConfirmDelete(order)" class="text-blue-500 hover:text-blue-700">
                             Confirm Order
                         </button>
                     </td>
                 </tr>
             </tbody>
         </table>
-
-        <!-- Confirmation Modal -->
-        <ConfirmationBox v-if="isModalVisible" title="Delete Product"
-            :message="`Are you sure you want to delete ${selectedProduct.name}?`" :isVisible="isModalVisible"
-            @confirm="deleteProduct" @cancel="closeModal" />
     </div>
+    <!-- Confirmation Modal -->
+    <ConfirmationBox v-if="isModalVisible" title="Confirm this order?"
+        :message="`Are you sure you want to confirm this order : ${selectedOrder.id}?`" :isVisible="isModalVisible"
+        @confirm="confirmOrder" @cancel="closeModal" />
 </template>
 
 <script setup>
@@ -63,34 +62,29 @@ onMounted(async () => {
 });
 
 const isModalVisible = ref(false);
-const selectedProduct = ref(null);
+const selectedOrder = ref(null);
 
-const showConfirmDelete = (product) => {
-    selectedProduct.value = product;
+const showConfirmDelete = (order) => {
+    selectedOrder.value = order;
     isModalVisible.value = true;
 };
 
 const closeModal = () => {
     isModalVisible.value = false;
-    selectedProduct.value = null;
+    selectedOrder.value = null;
 };
 
-const products = ref([
+const orders = ref([
     { id: 1, name: "John Doe", total: 499, total_item: 3, status: 'pending' },
     { id: 2, name: "Katie Jackson", total: 399, total_item: 1, status: 'confirmed' },
     { id: 3, name: "Stephanie", total: 259, total_item: 2, status: 'finish' },
     { id: 4, name: "Stepahanie", total: 419, total_item: 4, status: 'delivery' },
 ]);
 
-const editProduct = (product) => {
-    console.log('Edit product:', product);
-    // Logic to edit the product (e.g., navigate to an edit page or open a modal with product details)
-};
-
-const deleteProduct = () => {
-    var id = selectedProduct.value.id;
-    products.value = products.value.filter((product) => product.id !== id);
-    console.log('Product deleted:', id);
+const confirmOrder = () => {
+    var id = selectedOrder.value.id;
+    orders.value = orders.value.filter((order) => order.id !== id);
+    console.log('order deleted:', id);
     closeModal()
 };
 </script>
