89461ea0becb63949b3aabc26414cd3f6db06123
serkansyalcin <serkan.syalcin@hotmail.com>
2024-10-06 14:49:32 +0700

    finishing UI

--- resources/js/pages/admin/orders/OrderLists.vue
+++ resources/js/pages/admin/orders/OrderLists.vue
@@ -30,7 +30,8 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(order, index) in orderStore.orders" :key="order.id" class="hover:bg-gray-100">
+                <tr v-for="(order, index) in orderStore.orders" :key="order.id" class="hover:bg-gray-100 cursor-pointer"
+                    @click="showOrderDetail(order)">
                     <td class="px-6 py-4 border-b border-gray-300">{{ index + 1 }}</td>
                     <td class="px-6 py-4 border-b border-gray-300">{{ order.order_number }}</td>
                     <td class="px-6 py-4 border-b border-gray-300">
@@ -44,7 +45,7 @@
                         <span class="px-2 py-2 bg-blue-500 text-white rounded-md">{{ order.status }}</span>
                     </td>
                     <td class="px-6 py-4 border-b border-gray-300">
-                        <button @click="showConfirmDelete(order)" class="text-blue-500 hover:text-blue-700"
+                        <button @click="showConfirmOrder(order)" class="text-blue-500 hover:text-blue-700"
                             v-if="order.status == 'pending'">
                             Confirm Order
                         </button>
@@ -57,6 +58,10 @@
     <ConfirmationOrderBox v-if="isModalVisible" title="Confirm this order?"
         :message="`Are you sure you want to confirm this order : ${selectedOrder.order_number}?`"
         :isVisible="isModalVisible" @confirm="confirmOrder" @cancel="closeModal" :selectedOrder="selectedOrder" />
+
+    <!-- Confirmation Modal -->
+    <OrderDetailModal v-if="isModalDetailVisible" :isVisible="isModalDetailVisible" @cancel="closeModal"
+        :selectedOrder="selectedOrder" />
 </template>
 
 <script setup>
@@ -66,6 +71,7 @@ import ConfirmationOrderBox from "../../../components/ConfirmationOrderBox.vue";
 import router from "../../../router";
 import useOrder from '../../../store/order'
 import Swal from 'sweetalert2'
+import OrderDetailModal from "../../../components/OrderDetailModal.vue";
 
 const orderStore = useOrder();
 
@@ -75,16 +81,23 @@ onMounted(async () => {
 });
 
 const isModalVisible = ref(false);
+const isModalDetailVisible = ref(false);
 const selectedOrder = ref(null);
 
-const showConfirmDelete = (order) => {
+const showConfirmOrder = (order) => {
     selectedOrder.value = order;
     isModalVisible.value = true;
+
+};
+const showOrderDetail = (order) => {
+    selectedOrder.value = order;
+    isModalDetailVisible.value = true;
 };
 
 const closeModal = () => {
     isModalVisible.value = false;
     selectedOrder.value = null;
+    isModalDetailVisible.value = false;
 };
 
 const confirmOrder = () => {
